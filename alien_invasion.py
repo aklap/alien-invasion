a2f28e50968fbc77e1ee2aeebef641da3c426f3d
Alexis La Porte <aklaporte@gmail.com>
2018-12-19 01:49:06 -0500

    Draw ship to screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import sys  # NOTE: Methods to interact with command line.
 import pygame  # NOTE: Allows us to build our game
 
 from settings import Settings
-
+from ship import Ship
 
 def run_game():
     """Initialize game, create window object."""
@@ -12,6 +12,8 @@ def run_game():
     # Screen settings.
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    # Make a ship
+    ship = Ship(screen)
 
     # Start the run loop for our game.
     while True:
@@ -22,6 +24,9 @@ def run_game():
 
         # Redraw screen each pass of the loop and fill bg.
         screen.fill(ai_settings.bg_color)
+
+        # Draw ship
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
--- ship.py
+++ ship.py
@@ -1,23 +1,20 @@
 import pygame
 
+
 class Ship():
-
-
     def __init__(self, screen):
         """Initialize the ship and set its position on the screen."""
         self.screen = screen
 
-
         # Load ship image and get its rect.
-        self.image = pygame.image('images/ship.bmp')
-        self.rect = self.image.get_rect() # NOTE: rect is the HTML rect attribute
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()  # NOTE: rect is the HTML rect attribute
         self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-
-        def blitme(self):
-            """Draw the ship at its current location."""
-            self.screen.blit(self.image, self.rect)
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
