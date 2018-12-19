f139ceacd2e85429d0e88d5f6e10475208ec0f6e
Alexis La Porte <aklaporte@gmail.com>
2018-12-19 01:06:39 -0500

    Create pygame window.

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+import sys # NOTE: Methods to interact with command line.
+
+import pygame # NOTE: Allows us to build our game
+
+def run_game():
+    """Initialize game, create window object."""
+    pygame.init() # NOTE: Initialize a pygame instance.
+    screen = pygame.display.set_mode((1200, 800)) # NOTE: Set width and height of the display of the game in px, passing in a tuple.
+    pygame.display.set_caption('Alien Invasion')
+
+    # Start the run loop for our game.
+    while True:
+        for event in pygame.event.get(): # NOTE: Our event loop, watch key events.
+
+            if event.type == pygame.QUIT:
+                sys.ext() # NOTE: Exit the running process for our game, ie quit the game.
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+# Run the game :)
+run_game()
