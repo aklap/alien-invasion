d7b4bf7b57245504553cf46f827a499bc796e7e8
Alexis La Porte <aklaporte@gmail.com>
2018-12-28 03:22:10 -0500

    Remove bullets.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,11 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 # Run the game :)
