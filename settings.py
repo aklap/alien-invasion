280fe7d9fe13a9a1b53b4f57a3eb14ecdb38bfbc
Alexis La Porte <aklaporte@gmail.com>
2018-12-28 01:15:46 -0500

    Add  bullet settings.

--- settings.py
+++ settings.py
@@ -7,5 +7,10 @@ class Settings():
         self.screen_width = 1200  # in px
         self.screen_height = 800  # in px
         self.bg_color = (230, 230, 230)  # NOTE: Set bg color with tuple of values, RGB.
+        # Ship settings
         self.ship_speed_factor = 1.5  # Move the ship by 1.5 px
-
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60  # Dark gray
