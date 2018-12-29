7f596705bb12c81ef2f99fa5deaba9469fb7cb84
Alexis La Porte <aklaporte@gmail.com>
2018-12-29 13:17:39 -0500

    Limit number of bullets.

--- game_functions.py
+++ game_functions.py
@@ -23,8 +23,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         # Move the ship rect to the left.
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
