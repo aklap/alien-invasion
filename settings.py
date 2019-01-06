8a438503cdb4bc30b23fb0e128a4ce122e87dc45
Alexis La Porte <aklaporte@gmail.com>
2019-01-06 17:06:33 -0500

    Increase speed of bullet.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings():
         # Ship settings
         self.ship_speed_factor = 1.5  # Move the ship by 1.5 px
         # Bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)  # Dark gray
