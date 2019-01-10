66cac04428c35b5e30f81561cfa8a3aaf568b351
Alexis La Porte <aklaporte@gmail.com>
2019-01-09 23:52:41 -0500

    Fix missing screen_rect error.

--- button.py
+++ button.py
@@ -1,20 +1,22 @@
+import pygame
 import pygame.font
 
 
 class Button():
     """A class to represent a Button."""
+
     def __init__(self, ai_settings, screen, msg):
         """Initialize button attributes."""
         self.screen = screen
-        self.screen_rect == screen.get_rect()
-
+        self.screen_rect = screen.get_rect()
         # Set the dimensions and properties of the button
-        self.width, self.height = 200, 50
-        self.button.color = (0, 255, 0)
+        self.width = 200  # FIXME: This, width don't have an effect?
+        self.height = 50
+        self.button_color = (0, 0, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
         # Build the button's rect obj and center it
-        self.rect = Pygame.Rect(0, 0, self.width, self.height)
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
         # The button msg needs to be prepped only once
@@ -27,9 +29,6 @@ class Button():
         self.msg_image_rect.center = self.rect.center
 
     def draw_button(self):
-        """Draw the button"""
-        self.screen.fill(self.button_color, self.rect)
+        """Draw the button."""
+        # self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
-
-
-
