6ed2f0e2e00907d18400d0fa5d6be9fc25b1105d
Alexis La Porte <aklaporte@gmail.com>
2019-01-09 23:04:20 -0500

    Add stats.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame  # NOTE: Allows us to build our game
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import Stats as GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -23,14 +24,18 @@ def run_game():
     # Create the fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
+    # Initalize stats for a new game
+    stats = GameStats(ai_settings)
 
     # Start the run loop for our game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, aliens)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        else:
+            print('game is over')
 # Run the game :)
 run_game()
